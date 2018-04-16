4d43db3a868913f74251afd2a07ce912a246dd9d
Alexander C Whitehead <483446@hull.ac.uk>
2018-04-17 00:03:35 +0100

    changed smoke to be simpler and none tracked as requested by client

---
+++ Scenarios/Assets/Scripts/LowerSmoke.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class LowerSmoke : MonoBehaviour
+{	
+	// Update is called once per frame
+	void Update()
+	{
+		transform.position -= new Vector3(0.0f, 0.005f, 0.0f);
+	}
+}
---
+++ Scenarios/Assets/Scripts/SmokeStarter.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class SmokeStarter : MonoBehaviour
+{
+	public GameObject smoke;
+
+	void OnEnable()
+	{
+		smoke.SetActive (true);
+	}
+
+	void OnDisable()
+	{
+		smoke.SetActive (false);
+	}
+}
